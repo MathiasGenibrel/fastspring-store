7185aefb41f08b33e4d4ca4ed1f3254f6d997a70
Mathias Genibrel <mathias.geni@gmail.com>
2024-04-21 10:33:17 +0200

    feat: add new queries defaults options

--- entrypoints/popup/main.tsx
+++ entrypoints/popup/main.tsx
@@ -5,7 +5,14 @@ import "./style.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ChakraProvider } from "@chakra-ui/react";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 60, // 1 hour
+      refetchOnMount: false,
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
